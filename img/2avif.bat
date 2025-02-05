07d06da32cb9c85d4cb4524736fde79a1b5636a8
ciraos <frank.ka@foxmail.com>
2025-02-05 17:41:42 +0800

    updated via upload.

---
+++ img/2avif.bat
@@ -0,0 +1,16 @@
+@echo off
+
+::在下方设置要处理的视频或音频格式，这里列出了一些主要的视频格式
+set Ext=*.jpg,*.jpeg,*.png,*.webp
+
+echo 开始转换
+
+::在下方设置输出格式，这里输出为mp4，可自行更改
+for %%a in (%Ext%) do (
+	echo 正在转换：%%a
+	ffmpeg -loglevel quiet -i "%%a" -f avif "%%~na.avif" -y
+)
+
+echo 转换完成
+
+pause
